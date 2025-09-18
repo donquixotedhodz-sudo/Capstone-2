ab6a8556877116a2091317e2ec6e62ac8d1e04ce
Josh McDowell Trapal <donquixotedhodz@gmail.com>
2025-09-18 20:44:55 +0800

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,9 +1,9 @@
 <?php
 // Database configuration
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'db.fr-pari1.bengt.wasmernet.com');
 define('DB_NAME', 'fourjs_db');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_USER', 'bfbfab267c2f800012510f566a43');
+define('DB_PASS', '068cbfbf-ab26-7f5c-8000-e78d3a879836');
 
 // Set timezone to Philippines
 date_default_timezone_set('Asia/Manila');
